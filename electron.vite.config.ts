99a0abb6e9796b6ac106e89abe2390b35c628b99
xushengfeng <xushengfeng_zg@163.com>
2023-03-02 01:35:02 +0800

    主要 单窗口单页面，移除标签页管理

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from "electron-vite";
-import * as path from "path";
 
 export default defineConfig({
     main: {
         build: {
             rollupOptions: {
                 external: ["@electron-toolkit/utils"],
-            },
-        },
-    },
-    renderer: {
-        build: {
-            rollupOptions: {
-                input: {
-                    index: path.resolve(__dirname, "src/renderer/index.html"),
-                },
             },
         },
     },
--- src/main/main.ts
+++ src/main/main.ts
@@ -4,7 +4,6 @@ import { app, BrowserWindow, ipcMain, nativeTheme, BrowserView } from "electron"
 import * as path from "path";
 const run_path = path.join(path.resolve(__dirname, ""), "../../");
 import * as fs from "fs";
-import url from "node:url";
 
 // 自定义用户路径
 try {
@@ -33,31 +32,11 @@ if (process.argv.includes("-d") || import.meta.env.DEV) {
     dev = false;
 }
 
-/** 加载网页 */
-function renderer_path(window: BrowserWindow | Electron.WebContents, file_name: string, q?: Electron.LoadFileOptions) {
-    if (!app.isPackaged && process.env["ELECTRON_RENDERER_URL"]) {
-        let main_url = `${process.env["ELECTRON_RENDERER_URL"]}/${file_name}`;
-        let x = new url.URL(main_url);
-        if (q) {
-            if (q.search) x.search = q.search;
-            if (q.query) {
-                for (let i in q.query) {
-                    x.searchParams.set(i, q.query[i]);
-                }
-            }
-            if (q.hash) x.hash = q.hash;
-        }
-        window.loadURL(x.toString());
-    } else {
-        window.loadFile(path.join(__dirname, "../renderer", file_name), q);
-    }
-}
-
 app.commandLine.appendSwitch("enable-experimental-web-platform-features", "enable");
 app.commandLine.appendSwitch("disable-web-security");
 
 app.whenReady().then(() => {
-    create_main_window();
+    create_main_window("https://xlinkote.netlify.app/");
 });
 
 var the_icon = null;
@@ -76,164 +55,29 @@ var main_window_l: { [n: number]: BrowserWindow } = {};
  * @type {Object.<number, Array.<number>>}
  */
 var main_to_search_l: { [n: number]: Array<number> } = {};
-async function create_main_window() {
+async function create_main_window(url: string) {
     var window_name = new Date().getTime();
     var main_window = (main_window_l[window_name] = new BrowserWindow({
         backgroundColor: nativeTheme.shouldUseDarkColors ? "#0f0f0f" : "#ffffff",
         icon: the_icon,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            webSecurity: false,
-        },
         show: true,
-    })) as BrowserWindow & { html: string };
+    })) as BrowserWindow;
 
     main_to_search_l[window_name] = [];
 
     // 自定义界面
-    renderer_path(main_window, "index.html");
+    main_window.loadURL(url);
 
     if (dev) main_window.webContents.openDevTools();
 
-    main_window.on("close", () => {
-        for (let i of main_window.getBrowserViews()) {
-            // @ts-ignore
-            i?.webContents?.destroy();
-        }
+    main_window.webContents.setWindowOpenHandler(({ url }) => {
+        create_main_window(url);
+        return { action: "deny" };
     });
 
     main_window.on("closed", () => {
         delete main_window_l[window_name];
     });
 
-    main_window.webContents.on("found-in-page", (e, r) => {
-        main_window.webContents.send("found", r.activeMatchOrdinal, r.matches);
-    });
-
-    // 浏览器大小适应
-    main_window.on("resize", () => {
-        setTimeout(() => {
-            var [w, h] = main_window.getContentSize();
-            for (let i of main_window.getBrowserViews()) {
-                if (i.getBounds().width != 0) i.setBounds({ x: 0, y: 32, width: w, height: h - 32 });
-            }
-        }, 0);
-    });
-
     return window_name;
-}
-
-var search_window_l: { [n: number]: BrowserView } = {};
-ipcMain.on("open_url", (event, url) => {
-    create_browser(BrowserWindow.fromWebContents(event.sender), url);
-});
-
-/** 创建浏览器页面 */
-async function create_browser(window: BrowserWindow, url: string) {
-    var win_name = new Date().getTime();
-
-    let main_window = window;
-
-    if (main_window.isDestroyed()) return;
-    min_views(main_window);
-    var view = new Date().getTime();
-    let security = true;
-    var search_view = (search_window_l[view] = new BrowserView({ webPreferences: { webSecurity: security } }));
-    window.addBrowserView(search_view);
-    search_view.webContents.loadURL(url);
-    var [w, h] = main_window.getContentSize();
-    search_view.setBounds({ x: 0, y: 32, width: w, height: h - 32 });
-    main_window.setContentSize(w, h + 1);
-    main_window.setContentSize(w, h);
-    search_view.webContents.setWindowOpenHandler(({ url }) => {
-        create_browser(main_window, url);
-        return { action: "deny" };
-    });
-    if (dev) search_view.webContents.openDevTools();
-    if (!main_window.isDestroyed()) main_window.webContents.send("url", win_name, view, "new", url);
-    search_view.webContents.on("page-title-updated", (event, title) => {
-        if (!main_window.isDestroyed()) main_window.webContents.send("url", win_name, view, "title", title);
-    });
-    search_view.webContents.on("page-favicon-updated", (event, favlogo) => {
-        if (!main_window.isDestroyed()) main_window.webContents.send("url", win_name, view, "icon", favlogo);
-    });
-    search_view.webContents.on("did-navigate", (event, url) => {
-        if (!main_window.isDestroyed()) main_window.webContents.send("url", win_name, view, "url", url);
-    });
-    search_view.webContents.on("did-start-loading", () => {
-        if (!main_window.isDestroyed()) main_window.webContents.send("url", win_name, view, "load", true);
-    });
-    search_view.webContents.on("did-stop-loading", () => {
-        if (!main_window.isDestroyed()) main_window.webContents.send("url", win_name, view, "load", false);
-    });
-    search_view.webContents.on("did-fail-load", (event, err_code, err_des) => {
-        renderer_path(search_view.webContents, "browser_bg.html", {
-            query: { type: "did-fail-load", err_code: String(err_code), err_des },
-        });
-        if (dev) search_view.webContents.openDevTools();
-    });
-    search_view.webContents.on("render-process-gone", () => {
-        renderer_path(search_view.webContents, "browser_bg.html", { query: { type: "render-process-gone" } });
-        if (dev) search_view.webContents.openDevTools();
-    });
-    search_view.webContents.on("unresponsive", () => {
-        renderer_path(search_view.webContents, "browser_bg.html", { query: { type: "unresponsive" } });
-        if (dev) search_view.webContents.openDevTools();
-    });
-    search_view.webContents.on("responsive", () => {
-        search_view.webContents.loadURL(url);
-    });
-    search_view.webContents.on("certificate-error", () => {
-        renderer_path(search_view.webContents, "browser_bg.html", { query: { type: "certificate-error" } });
-        if (dev) search_view.webContents.openDevTools();
-    });
-}
-
-ipcMain.on("tab_view", (e, id, arg, arg2) => {
-    let main_window = BrowserWindow.fromWebContents(e.sender);
-    let search_window = search_window_l[id];
-    switch (arg) {
-        case "close":
-            main_window.removeBrowserView(search_window);
-            // @ts-ignore
-            search_window.webContents.destroy();
-            delete search_window_l[id];
-            if (main_window.getBrowserViews().length == 0) {
-                main_window.close();
-            }
-            break;
-        case "top":
-            // 有时直接把主页面当成浏览器打开，这时pid未初始化就触发top了，直接忽略
-            if (!main_window) return;
-            main_window.setTopBrowserView(search_window);
-            min_views(main_window);
-            search_window.setBounds({
-                x: 0,
-                y: 32,
-                width: main_window.getContentBounds().width,
-                height: main_window.getContentBounds().height - 32,
-            });
-            break;
-        case "stop":
-            search_window.webContents.stop();
-            break;
-        case "reload":
-            search_window.webContents.reload();
-            break;
-        case "save_html":
-            main_window["html"] = arg2;
-            min_views(main_window);
-            break;
-        case "dev":
-            search_window.webContents.openDevTools();
-            break;
-    }
-});
-
-/** 最小化某个窗口的所有标签页 */
-function min_views(main_window: BrowserWindow) {
-    for (let v of main_window.getBrowserViews()) {
-        v.setBounds({ x: 0, y: 0, width: 0, height: 0 });
-    }
 }
