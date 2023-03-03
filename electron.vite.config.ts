158930c4d302b7a555beec5519b6c64fc8ff1e4c
xushengfeng <xushengfeng_zg@163.com>
2023-03-03 22:56:30 +0800

    主要 添加设置页面

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "electron-vite";
+import * as path from "path";
 
 export default defineConfig({
     main: {
         build: {
             rollupOptions: {
                 external: ["@electron-toolkit/utils"],
+            },
+        },
+    },
+    renderer: {
+        build: {
+            rollupOptions: {
+                input: {
+                    setting: path.resolve(__dirname, "src/renderer/setting.html"),
+                },
             },
         },
     },
